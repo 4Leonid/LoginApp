45bbcec5ee22505e5f37a7aa627dcbec53266e69
4Leonid <leo-turko@mail.ru>
2022-03-12 17:56:10 +0300

    Code optimisation

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -11,27 +11,38 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet var welcomeUserLabel: UILabel!
     
-    var userName: String?
+    var userName = ""
+    
+    private let coloreOne = UIColor(
+        red: 0.2,
+        green: 0.6,
+        blue: 0.3,
+        alpha: 1
+    )
+    
+    private let coloreTwo = UIColor(
+        red: 0.4,
+        green: 0.2,
+        blue: 0.6,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomeUserLabel.text = "Welcome, \(userName ?? "")!"
-        
-        let coloreOne = UIColor(red: 0.2, green: 0.6, blue: 0.3, alpha: 1.0).cgColor
-        let coloreTwo = UIColor(red: 0.4, green: 0.2, blue: 0.6, alpha: 1.0).cgColor
-        
+        view.addGradientColore(topColore: coloreOne, bottomColore: coloreTwo)
+        welcomeUserLabel.text = "Welcome, \(userName)!"
+    }
+}
+
+//MARK: - Set background color
+extension UIView {
+    func addGradientColore(topColore: UIColor, bottomColore: UIColor) {
         let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [coloreOne, coloreTwo]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        view.layer.insertSublayer(gradientLayer, at: 0)
-        
+        gradientLayer.frame = bounds
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.colors = [topColore.cgColor, bottomColore.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-    @IBAction func logoutButtonPressed() {
-        dismiss(animated: true)
-    }
-    
 }
