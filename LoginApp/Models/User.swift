19a48789b5c97cd4ed19d4831d6ec2732d9fbfec
4Leonid <leo-turko@mail.ru>
2022-03-13 19:57:58 +0300

    Model creation

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by EkaterinaP on 12.03.2022.
+//
+
+import Foundation
+import UIKit
+
+struct User {
+    let login: String
+    let password: String
+    var persons: Person
+    
+    static func getStandartUser() -> User {
+            User(login: "Leo", password: "1", persons: Person(
+            firstName: "Leonid",
+            secondName: "Turko",
+            imageName: "Swift",
+            work: "Programmer",
+            age: 37))
+    }
+    
+    //static let standart: User = User(login: "Leo", password: "1", persons: Person(firstName: "Leo", secondName: "Turko", work: "Programmer", age: 37))
+}
+
+struct Person {
+    let firstName: String
+    let secondName: String
+    let imageName: String
+    let work: String
+    let age: Int
+    
+}
